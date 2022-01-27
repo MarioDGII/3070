9821615525e4ee218cbd81eb4ca7d1da9d7cfc06
COOK-INFS <44375801+COOK-INFS@users.noreply.github.com>
2022-01-27 12:22:10 -0700

    Added example of commenting in code.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,21 @@
+/*
+ * Just my first program to 
+ * demonstrate how good I am
+ * at programming in Java with Eclipse.
+ */
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 	
-		System.out.println("Hello World AGAIN!");
-
+		// Code to display message "Hello World" in console.
+		
+		System.out.println("Hello World!");
+		
+		/*
+		System.out.println("How are you doing today?");
+		System.out.println("Another line!");
+		*/
 	}
 
 }
